1fb116cb2b2a9fb4dcf2905f9f8c9b50409e2b41
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2018-03-07 10:18:12 +0200

    Update Directory.py

--- Directory.py
+++ Directory.py
@@ -2,35 +2,37 @@ from threading import Thread
 from Crypto.PublicKey import RSA
 import socket
 from thread import *
+import random
+
+"""keylist = {
+        'Client': RSA.generate(1024),
+        'Server1': RSA.generate(1024),
+        'Server2': RSA.generate(1024),
+        'Public_key': RSA.generate(1024)
+        }"""
 
 new_key = RSA.generate(1024)
 servers = []
 
-def tostr(servers):
-        keys = ""
-        for o in range(len(servers)):
-                for i in range(len(servers[o])):
-                        keys += servers[o][i] + "###"
+def choose_order():
+        num = random.randint(0, (len(servers) - 1))
+        keys = servers[num][0] + "###" + servers[num][1]
+        servers.remove(servers[num])
+        print keys
         return keys
 
 def client_thread(conn):
-        #conn.send("Do you want keys?")
         while True:
                 data = str(conn.recv(1024))
                 if str(data) == "gimmekeys":
-                        conn.send(tostr(servers))
+                        conn.send(choose_order())
                 elif data.startswith("onionnode"):
-                        print("Yhhdddssttyyy")
                         data = data.split("###")
                         servers.append(data[1:3])
-                        print(servers)
-                #conn.send(keylist['Client'].exportKey('PEM'))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 6668))
 server.listen(4)
-
-print(tostr(servers))
 
 while 1:
         conn, addr = server.accept()
@@ -38,4 +40,3 @@ while 1:
         start_new_thread(client_thread, (conn,))
 
 server.close()
-
