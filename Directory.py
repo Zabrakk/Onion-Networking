9f9f6e9d25c4eadcdfd0feca37c2a730c6c4db13
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2018-03-12 20:18:59 +0200

    Update Directory.py

--- Directory.py
+++ Directory.py
@@ -4,21 +4,15 @@ import socket
 from thread import *
 import random
 
-"""keylist = {
-        'Client': RSA.generate(1024),
-        'Server1': RSA.generate(1024),
-        'Server2': RSA.generate(1024),
-        'Public_key': RSA.generate(1024)
-        }"""
-
 new_key = RSA.generate(1024)
-servers = []
+send_keys = []
+return_keys = []
 
 def choose_order():
         num = random.randint(0, (len(servers) - 1))
         keys = servers[num][0] + "###" + servers[num][1]
         servers.remove(servers[num])
-        print keys
+        print(keys)
         return keys
 
 def client_thread(conn):
@@ -26,9 +20,13 @@ def client_thread(conn):
                 data = str(conn.recv(1024))
                 if str(data) == "gimmekeys":
                         conn.send(choose_order())
-                elif data.startswith("onionnode"):
+                elif data.startswith("sendkeys"):
                         data = data.split("###")
-                        servers.append(data[1:3])
+                        send_keys.append(data[1:3])
+                elif data.strtswith("returnkeys"):
+                        data = data.split("###")
+                        return_keys.append(data[1:3])
+                        
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 6668))
@@ -36,7 +34,7 @@ server.listen(4)
 
 while 1:
         conn, addr = server.accept()
-        print 'Connected from: ' + addr[0] + ':' + str(addr[1])
+        print ('Connected from: ' + addr[0] + ':' + str(addr[1]))
         start_new_thread(client_thread, (conn,))
 
 server.close()
