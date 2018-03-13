1c10858691491fb234cd67938ad0d8f97b55513d
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2018-03-13 06:24:34 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,17 +2,27 @@ from threading import Thread
 from Crypto.PublicKey import RSA
 import socket
 from thread import *
+import random
 
 DIR_PORT = 6668
 HOST = "localhost"
 NODEKEYS = []
-MESSAGE = "Somethinggood"
-CLINET_PORT = 5556
+CLIENT_PORT = 5556
+MESSAGE = "sometinggood###" + str(CLIENT_PORT)
 
 def encrypt(key, port):
-    publicKeyObj = RSA.importKey(key)
-    MESSAGE = publickeyObj.encrypt(MESSAGE, 32)[0] + "###" + port
+        publickeyObj = RSA,importKey(key)
+        MESSAGE = publickeyObj.encrypt(MESSAGE, 32)[0] + "###" + str(CLIENT_PORT)
 
-def decypt(message);
-    while NODEKEYS:
-        key = PODEKEYS.pop()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((HOST, DIR_PORT))
+while len(NODEKEYS) < 3:
+        server.send("sendreturn")
+        message = server.recv(1024)
+        print(message)
+        key, port = str(message).split("###")
+        #encyptt(key, port)
+        NODEKEYS.append(key)
+
+print NODEKEYS
+server.close()
