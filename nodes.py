88c0cc3755aee309c0b292514c0b4193d7711182
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2018-03-28 18:44:33 +0300

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,9 +1,11 @@
 import socket
 from Crypto.PublicKey import RSA
 from threading import Thread
-from base64 import b64decode
+from Crypto.Cipher import AES
+import random
+import string
 
-DIRPORT = 6668
+DIRPORT = 6667
 ports = [6555, 6444, 6333]
 
 class Node:
@@ -11,10 +13,14 @@ class Node:
                 self.node = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.node.bind((host, port))
                 self.node.listen(10)
-                self.publickey = ""
-                self.privatekey = ""
+                self.send_publickey = ""
                 self.port = port
-                
+                #AES
+                self.key = random.choice(string.letters) * 16
+                self.IV = 16 * '\x00'
+                self.mode = AES.MODE_CBC
+                self.encryptor = AES.new(self.key, self.mode, IV=self.IV)
+
         def make_keys(self):
                 new_sendkey = RSA.generate(1024)
                 new_returnkey = RSA.generate(1024)
@@ -23,38 +29,30 @@ class Node:
                 self.return_publickey = new_returnkey.publickey().exportKey('PEM')
                 self.return_privatekey = new_returnkey.exportKey('PEM')
 
-         def decrypt(self, message):
-                raw_data = b64decode(message)
-                decrypted = self.return_key.decrypt(raw_data)
-                print decrypted
-                return decypted
+        def decrypt(self, message):
+                decryptor = AES.new(self.key, self.mode, IV=self.IV)
+                return str(decryptor.decrypt(message))
 
-                
         def send_key_port(self):
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect(('localhost', DIRPORT))
-                s.send("sendkeys###" + self.send_publickey + "###" + str(self.port))
+                s.send("send_key###" + self.send_publickey + "###" + str(self.port))
                 print("sending send keys")
                 s.close()
-                
+
         def return_key(self):
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect(('localhost', DIRPORT))
-                s.send("returnkeys###" + self.return_publickey + "###" + str(self.port))
-                print "sending return keys"
-                s.close()
-
+                s.send("return_key###" + self.return_publickey + "###" + str(self.port))
 
 
         def mainloop(self):
                 self.make_keys()
                 self.send_key_port()
-                self.return_key()
+                #self.return_key()
                 #while True:
                 #       conn, addr = self.node.accept()
                 #       print("Connected with " + addr[0] + " : " + str(addr[1]))
-                
+
 for i in range(3):
         Thread(target = Node('localhost', ports[i]).mainloop).start()
-#Thread(target = Node('localhost', 6444).mainloop).start()
-#Thread(target = Node('localhost', 6333).mainloop).start()
