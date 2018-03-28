c11189347a841367e0df6ec8d5851ae0f237e447
Zabrakk <32382184+Zabrakk@users.noreply.github.com>
2018-03-28 18:45:36 +0300

    Update Directory.py

--- Directory.py
+++ Directory.py
@@ -4,44 +4,41 @@ import socket
 from thread import *
 import random
 
-new_key = RSA.generate(1024)
 send_keys = []
 return_keys = []
+sent = []
+dir = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dir.bind(('localhost', 6667))
+dir.listen(10)
 
-def choose_order():
-        if  action == "send":
-                num = random.randint(0, (len(send_keys) - 1))
-                keys = send_keys[num][0] + "###" + send_keys[num][1]
-                send_keys.remove(send_keys[num])
-        elif action == "return":
-                num = random.randint(0, (len(return_keys) -1))
-                keys = return_keys[num][0] + "###" + return_keys[num][1]
-                return_keys.remove(return_keys[num])
-        print keys
-        return keys
-
-
-def client_thread(conn):
+def connection_threads(conn):
         while True:
                 data = str(conn.recv(1024))
-                if str(data) == "gimmekeys":
-                        conn.send(choose_order("send"))
-                elif str(data) == "sendreturn":
-                        conn.send(choose_order("return"))
-                elif data.startswith("sendkeys"):
+                if data == "give_send": #Send keys to client
+                        conn.send(choose_send_order(send_keys))
+                elif data == "give_return": #Send return keys to node
+                        conn.send(coohse_order(return_keys))
+                elif data.startswith("send_key"): #Get keys from nodes
                         data = data.split("###")
                         send_keys.append(data[1:3])
-                elif data.startswith("returnkeys"):
+                elif data.startswith("return_key"):
                         data = data.split("###")
                         return_keys.append(data[1:3])
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 6668))
-server.listen(5)
+def choose_send_order(keylist):
+        #Generate random order for nodes
+        num = random.randint(0, (len(send_keys) - 1))
+        keys  = send_keys[num][0] + "###" + send_keys[num][1]
+        send_keys.remove(send_keys[num])
+        sent.append(keys)
+        return keys
 
-while 1:
-        conn, addr = server.accept()
-        print 'Connected from: ' + addr[0] + ':' + str(addr[1])
-        start_new_thread(client_thread, (conn,))
+def send_return_keys():
+        return False
+
+while True:
+        conn, addr = dir.accept()
+        #print "Connected from: " + addr[0] + ":" + str(addr[1])
+        start_new_thread(connection_threads, (conn,))
 
 server.close()
