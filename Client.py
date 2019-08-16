93e8dba5efe639dd7370f36de43848ed328f602e
hennas <32382243+hennas@users.noreply.github.com>
2019-08-16 11:12:34 +0300

    Revised version

--- Client.py
+++ Client.py
@@ -1,89 +1,137 @@
-from Crypto.PublicKey import RSA
 import socket
-from thread import *
+import threading
+from AES_func import AES_func
+from diffiesecret import *
 from Crypto.Cipher import AES
-import random
-import string
 
-DIR_PORT = 6667
-CLIENT_PORT = 6660
+#Connection config
 HOST = "localhost"
-keys = []
-return_keys = []
-send_data = []
-message = "Hello worldddddd###6789" #Number is servers port
-#Message must be 16bit for PyCrypto AES
+DIR_PORT = 6677 #Directories port, connect here
+CLIENT_PORT = 6600 #Client own port
+NODE_PORT = 0 #Init this variable
 
-#Ei turvallista
-key = 'a' * 16
-IV = 16 * '\x00'
-mode = AES.MODE_CBC
-encryptor = AES.new(key, mode, IV=IV)
+#DH
+OWNSECRET = 15 #Secret number for Diffie-Hellman
 
-def encrypt_message(): #Asymmetric keys for fowarding
-        key = RSA.importKey(keys[0][0])
-        first_encrypt = str(key.encrypt(message, 32)) + "###" + return_keys[0][0] + "###" + return_keys[0][1] + "###" + str(return_keys[0][2])+ "###"  + keys[0][1]
-        second_encrypt = str(key.encrypt(first_encrypt, 32)) + "###" + return_keys[1][0] + "###" + return_keys[1][1]+ "###" + str(return_keys[1][2]) + "###" + keys[1][1]
-        third_encrypt = str(key.encrypt(second_encrypt, 32)) +  "###" + return_keys[2][0] + "###" + return_keys[2][1]+ "###" + str(return_keys[2][2]) + "###" + keys[2][1]
-        print third_encrypt
-        split_encrypted(third_encrypt)
-        #for i in range(1, len(keys)):
-        #       key = RSA.importKey(keys[i][0])
-        #       encrypted_message += str(key.encrypt(encrypted_message, 32)) + "###" + keys[i][1] +  "###" + return_keys[i][0] + "###" + return_keys[i][1]+ "###" + str(return_keys[i][2])
-        #       print encrypted_message
-        #       print
-        #return encrypted_message
+#AES
+AES = AES_func() #Create AES_func object for encryption/decryption
+DIR_KEY = 0 #Init this variable
+KEYS = [] #Last node, ..., first node
 
-def create_return(): #Seeds for node keys
-        for i in range(3):
-                key = random.choice(string.letters) * 16
-                print key
-                IV = 16 * '\x00'
-                mode = AES.MODE_CBC
-                return_keys.append([key, IV, mode])
-                print return_keys
-        print
+PAGES = ["youtube.com", "memecenter.com", "vauva.fi"] #"Websites" we can connect to
+SERVER_PORTS = ["7777", "7799", "7788"] #And their ports
 
-def split_encrypted(encrypted): #Port, char*16, IV, AES.mode.
-        print str(encrypted).split("###")
-        data = encrypted.split("###")
-        send_data.append(data[1:])
-        print
-        print send_data
-        send_message(encrypted, send_data)
+def create_socket():
+	'''
+	Input: None
+	Output: Socket
+	For creating sockets
+	'''
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	return sock
 
-def decrypt_message(message): #Symmetric key for return
-        decryptor = AES.new(key, mode, IV=IV)
-        return str(decryptor.decrypt(message))
+def dir_connect():
+	'''
+	Input: None
+	Output: None
+	Connects to the directory to get a encryption key by performing the DH key exchange
+	'''
 
-def send_key():
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((HOST, DIR_PORT))
-        client.send("return_key###" + public_key + "###" + str(CLIENT_PORT))
-        print("Sending key to directory")
-        client.close()
+	global DIR_KEY
+	print("#: Connecting to directory")
+	sock = create_socket()
+	sock.connect((HOST, DIR_PORT))
 
-def get_keys():
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((HOST, DIR_PORT))
-        while len(keys) < 3:
-                client.send("give_send")
-                data = str(client.recv(1024))
-                key, port = data.split("###")
-                keys.append([key, port])
+	#DH
+	sock.send(calculate_shared(OWNSECRET).encode("utf-8"))
+	SECRET = calculate_secret(sock.recv(1024), OWNSECRET)
+	print(SECRET)
 
-def send_message(message, data):
-        #client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #client.connect((HOST, data[0]))
-        #client.send(message)
-        print message[:-7]
+	DIR_KEY = AES.DF_keygen(SECRET)
+	print(DIR_KEY)
+	sock.close()
 
-get_keys()
-create_return()
-encrypt_message()
-#print public_key
-#print private_key
-#send_key()
+def recieve_keys():
+	'''
+	Input: None
+	Output: None
+	Recieves AES keys that the nodes use and the first nodes port
+	'''
+	global NODE_PORT, KEYS
+	sock = create_socket()
+	sock.bind((HOST, CLIENT_PORT))
+	sock.listen(2)
+	conn, addr = sock.accept()
+
+	message = conn.recv(1024) #key0, key1, key2, node_port
+	message = AES.decrypt(message, DIR_KEY).split("###")
+	KEYS = [message[i] for i in range(3)]
+	NODE_PORT = message[3]
+	print(KEYS, NODE_PORT)
+
+def select_page():
+	'''
+	Input: None
+	Output: A tuple containing chosen page and its port
+	Loops until user enters a suitable option
+	'''
+	opt = 0
+	try:
+		while int(opt) not in [1,2,3]:
+			opt = raw_input("Choose page, 1: Youtube, 2: Memecenter, 3: Vauva.fi: ")
+			return PAGES[int(opt) - 1], SERVER_PORTS[int(opt) - 1]
+	except:
+		print("Enter a valid number")
+		select_page()
 
 
-print decrypt_message(encryptor.encrypt(message[0:16]))
+def encrypt_msg(target):
+	'''
+	Input: Clients message, in this case the tuple obtained from select_page
+	Output: Encrypted message that is sent to the first node
+	Uses each key ones to encrypt the message
+	Adds ### betweem the tuples object, plaint text messages split from those
+	'''
+	msg = target[0] + "###" + target[1]
+	for key in KEYS:
+		#print(msg)
+		msg = AES.encrypt(msg, key)
+	print(msg)
+	return msg
+
+def decrypt_msg(msg):
+	'''
+	Input: Message from server
+	Output: Decrypted message
+	Uses each node key in a reversed order to decrypt the message
+	'''
+	for key in reversed(KEYS):
+		msg = AES.decrypt(msg, key)
+	print(msg)
+
+def send_msg_to_node(msg):
+	'''
+	Input: Encrypted message
+	Output: None
+	Send the message to first node
+	'''
+	sock = create_socket()
+	sock.connect((HOST, int(NODE_PORT)))
+	sock.send(msg.encode("utf-8"))
+	return sock.recv(1024).decode("utf-8")
+	#sock.close()
+
+def main():
+	'''
+	Calls each function in order, thats all
+	'''
+	dir_connect()
+	recieve_keys()
+	target = select_page()
+	msg = encrypt_msg(target)
+	answer = send_msg_to_node(msg)
+	decrypt_msg(answer)
+
+if __name__ == "__main__": #Start
+	main()
