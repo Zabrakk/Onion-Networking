254b1163929c9b4e400f21bab5df0d685526f228
hennas <32382243+hennas@users.noreply.github.com>
2019-08-16 11:14:46 +0300

    Revised version

--- Server.py
+++ Server.py
@@ -1,28 +1,40 @@
-from threading import Thread
-from Crypto.PublicKey import RSA
 import socket
-from thread import *
-import random
+from threading import Thread
 
-DIR_PORT = 6668
 HOST = "localhost"
-NODEKEYS = []
-CLIENT_PORT = 5556
-MESSAGE = "sometinggood###" + str(CLIENT_PORT)
 
-def encrypt(key, port):
-        publickeyObj = RSA,importKey(key)
-        MESSAGE = publickeyObj.encrypt(MESSAGE, 32)[0] + "###" + str(CLIENT_PORT)
+class Server:
+	def __init__(self,port,page_name,answer):
+		self.PORT = port #Server port
+		self.PAGE_NAME = page_name #Which "website" the server is
+		self.ANSWER = answer #Predetermined answer the server gives to its client
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((HOST, DIR_PORT))
-while len(NODEKEYS) < 3:
-        server.send("sendreturn")
-        message = server.recv(1024)
-        print(message)
-        key, port = str(message).split("###")
-        #encyptt(key, port)
-        NODEKEYS.append(key)
+	def create_socket(self):
+		'''Creates sockets'''
+		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		return server
 
-print NODEKEYS
-server.close()
+	def server_listen(self):
+		'''Does what the server is supposed to do'''
+		server = self.create_socket()
+		server.bind((HOST, self.PORT))
+		server.listen(1)
+		conn, addr = server.accept()
+		msg = str(conn.recv(1024))
+		if msg == self.PAGE_NAME: #If client asked the right page
+			conn.send(self.ANSWER.encode('utf-8'))
+		else: #If it was something else
+			conn.send('404 Not Found'.encode('utf-8'))
+
+	def main(self):
+		self.server_listen()
+
+
+
+ports = [7777, 7788, 7799]
+sites = ['youtube.com', 'vauva.fi', 'memecenter.com']
+answers = ['This is youtube', 'This is troll forum', 'This is Sparta']
+for i in range(3):
+	Thread(target = Server(ports[i],sites[i],answers[i]).main).start()
+
